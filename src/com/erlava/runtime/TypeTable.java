9cfd87e96b4978bb66b2a9ad74ad40c67c21fa56
hexaredecimal <mfanakagama@gmail.com>
2024-06-21 21:31:04 +0200

    runtime: A symbol table for user defined types

---
+++ src/com/erlava/runtime/TypeTable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.erlava.runtime;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class TypeTable {
+	public static HashMap<String, ArrayList<String>> types = new HashMap<>();
+}
