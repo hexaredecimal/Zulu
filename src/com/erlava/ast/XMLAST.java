d69ca52997aca26f2144b6a05dfc7e7a5f71faa7
hexaredecimal <mfanakagama@gmail.com>
2024-06-23 09:09:48 +0200

    ast: The AST for XML Expressions

---
+++ src/com/erlava/ast/XMLAST.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.erlava.ast;
+
+import com.erlava.optimizations.Optimization;
+import com.erlava.runtime.BarleyAtom;
+import com.erlava.runtime.BarleyString;
+import com.erlava.runtime.BarleyValue;
+import com.erlava.utils.AST;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class XMLAST implements AST, Serializable{
+	ArrayList<AST> nodes; 
+
+	public XMLAST(ArrayList<AST> nodes) {
+		this.nodes = nodes;
+	}
+	
+	@Override
+	public BarleyValue execute() {
+		String txt = ""; 
+		for (AST node: nodes) {
+			if (node instanceof StringAST) {
+				BarleyString s = (BarleyString) node.execute();
+				for (byte b: s.getString()) {
+					txt += (char)b;
+				}
+			} else {
+				txt += node.execute().toString();
+			}
+		}
+		System.out.println("" + txt);
+		return new BarleyAtom("ok");
+	}
+
+	@Override
+	public void visit(Optimization optimization) {
+	}
+}
