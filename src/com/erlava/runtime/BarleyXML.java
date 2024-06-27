13028ecc4aa451d3562cf33a38c68802c563bbe2
hexaredecimal <mfanakagama@gmail.com>
2024-06-27 20:36:06 +0200

    runtime: Use Dom elements for the XML and also implements a prettyprinter

--- src/com/erlava/runtime/BarleyXML.java
+++ src/com/erlava/runtime/BarleyXML.java
@@ -6,10 +6,24 @@ package com.erlava.runtime;
 
 import com.erlava.utils.BarleyException;
 import java.io.Serializable;
+import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import xmlparser.model.XmlElement;
 
 /**
@@ -18,35 +32,56 @@ import xmlparser.model.XmlElement;
  */
 public class BarleyXML implements BarleyValue, Serializable {
 
-	private XmlElement element;
+	private Element element;
 
-	public BarleyXML(XmlElement element) {
+	public BarleyXML(Element element) {
 		this.element = element;
 	}
 
+
+	public String prettyPrint() {
+		try {
+			// Create a TransformerFactory
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			// Create a Transformer
+			Transformer transformer = transformerFactory.newTransformer();
+
+			// Set output properties to pretty print
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+
+			// Create a StringWriter to capture the output
+			StringWriter writer = new StringWriter();
+
+			// Transform the document to a string
+			transformer.transform(new DOMSource(element), new StreamResult(writer));
+
+			// Print the pretty-printed XML string
+			String prettyPrintedXml = writer.toString();
+			return prettyPrintedXml; 
+		} catch (TransformerConfigurationException ex) {
+			Logger.getLogger(BarleyXML.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (TransformerException ex) {
+			Logger.getLogger(BarleyXML.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return null; 
+	}
+	
 	public BarleyValue getNodeByTagName(String tag) {
-		List<XmlElement> elements = this.element.getElementsByTagName(tag);
+		NodeList elements = this.element.getElementsByTagName(tag);
 		LinkedList<BarleyValue> obj = new LinkedList<>(); 
-		for (XmlElement e: elements) {
-			BarleyXML xml = new BarleyXML(e);
-			obj.add(xml);
+		for (int i = 0; i < elements.getLength(); i++) {
+			Node n = elements.item(i);
+			if (n.getNodeType() == Node.ELEMENT_NODE) {
+				obj.push(new BarleyXML((Element) n));
+			}
 		}
 		return new BarleyList(obj);
 	}
 
 	public BarleyValue getNodeByTagId(String id) {
-		List<XmlElement> elements = this.element.children;
-		LinkedList<BarleyValue> obj = new LinkedList<>(); 
-		for (XmlElement e: elements) {
-			if (e.attributes.containsKey("id")) {
-				String val = e.attributes.get("id");
-				if (id.equals(val)) {
-					BarleyXML xml = new BarleyXML(e);
-					obj.add(xml);
-				}
-			}
-		}
-		return new BarleyList(obj);
+		return new BarleyAtom("error");
 	}
 	
 	@Override
