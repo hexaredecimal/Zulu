988853f1fe68811964bb4f72faef0441ad4df53d
hexaredecimal <mfanakagama@gmail.com>
2024-07-06 20:01:32 +0200

    erlava: small refactoring that comments out the test runner (WIP)

--- src/com/erlava/Main.java
+++ src/com/erlava/Main.java
@@ -13,9 +13,15 @@ import com.erlava.utils.SourceLoader;
 import java.io.IOException;
 import java.util.LinkedList;
 
+
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		
+		//Test.run();
+		//System.exit(0);
+		
 		ArgParser argp = new ArgParser(args);
 		Config conf = new Config();
 		conf.setProgram("Erlava");
