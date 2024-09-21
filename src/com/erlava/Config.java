01fc6a1b34e25000e1549d02f8792045ad19b530
hexaredecimal <mfanakagama@gmail.com>
2024-09-21 17:26:04 +0200

    config: Make main.lava load by default

--- src/com/erlava/Config.java
+++ src/com/erlava/Config.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.erlava;
 
 import java.io.File;
@@ -35,7 +31,7 @@ public class Config {
 		this.repl = r;
 		this.name = "";
 		this.test = false;
-		this.entry = "";
+		this.entry = "main.lava";
 		this.entry_module = "main";
 	}
 
